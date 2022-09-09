922658720b1ff4c73fa2aa91db8ca8ef512ae97c
francoBoero <franboero2002@gmail.com>
2022-09-08 22:03:41 -0400

    cambio

--- app.js
+++ app.js
@@ -11,10 +11,80 @@ let nombreCompleto = nombre + " " + apellido
         alert("bienvenido don nadie")
         
     }
+    /*let ellecion = prompt("elija alguna de esta opcion: prdocuto 1:TV de 45 pulgadas, precio:50000. Prducto2:  zapatllas nike jordan4, precio:30000.  Producto3: remera del psg, precio:15000")
+    
+    
+    if (eleccion === 1){
+
+    }
+   */
+  let toal = 0
+ /* const productos = [
+    {id: 1, prdocuto: "TV de 45 pulgadas", precio:50000},     {id: 2, prducto: " zapatllas nike jordan4", precio:30000}, {id: 3, producto: "remera del psg", precio:15000},
+            ]*/
+           
+    carrito = []
+    
+    function compra(){
+        let uno = {id: 1, prdocuto: "TV de 45 pulgadas", precio:50000}
+        let dos = {id: 2, prducto: " zapatllas nike jordan4", precio:30000}
+        let tres = {id: 3, producto: "remera del psg", precio:15000}
+        let option =  prompt(`elija un producto:
+        uno: TV de 45 pulgadas $50000
+        dos: zapatllas nike jordan4 $30000
+        tres:remera del psg $15000
+        si no desea nada escriba: NO`);
+        switch(option){
+            case "uno": 
+            return carrito.push(uno);
+                alert("se ha agregado un producto a su carrito");
+               
+
+                break;
+            case "dos": 
+            return carrito.push(dos);
+                  alert("se ha agregado un producto a su carrito");
+                
+                break;
+            case "tres": 
+            return carrito.push(tres);
+            alert("se ha agregado un producto a su carrito");
+            
+                break;
+            
+            default: return alert("re seco andas o que?")   
+                break;
+    
+        }
+    }
+    compra()
+ 
+ 
+ 
+
+    for (const producto of carrito){
+        console.log(carrito.id);
+        console.log(carrito.prodcuto);
+        console.log(carrito.precio)
+    
+    }
+        
+   
+ let total = carrito.forEach((producto)=>(precio += carrito.precio))
+    alert (`el total a pagar es de: ${total}`)
+    
+   
+    
+    
+  
+    
+    
+
     
 
 
 
+/*
  function compra(){
     let uno = (50000)
     let dos = (30000)
@@ -44,15 +114,9 @@ let nombreCompleto = nombre + " " + apellido
     
     }
     compra()
+    */
+
+
+
     
-
-let comentario = prompt("desea dejenos su comentario acerca del servicio")
-
-
- while(comentario != " " && comentario != "esc" && comentario!= "ESC"){
-  console.log( "gracias por tu comentario" + " " + nombre);
-  comentario = prompt("deje otro comentario sino presione ESC");
- }
-
-
 
